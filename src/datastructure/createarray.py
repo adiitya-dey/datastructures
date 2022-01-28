de0b0eeaae290c9cc6fc2581d1b113e19c6d90ee
Aditya Dey <aditya.dey@nmbu.no>
2022-01-28 00:59:13 +0000

    Added docstring for sphinx.

--- src/datastructure/createarray.py
+++ src/datastructure/createarray.py
@@ -2,9 +2,16 @@ import numpy as np
 import logging
 
 class CreateArray:
+    """
+    This class creates a numpy array which is used by algorithms for sorting.
+    """
     
     def __init__(self, __size, __atype):
-        
+        """
+        This function generates a numpy array of mention size and type like sorted, random or reverse.
+        :param size: int
+        :param atype: str
+        """
         #Create Array based on Type: Sorted, Reverse, Random
         if __atype.lower() == "sorted":
             self.__array = np.arange(1, __size+1)
@@ -15,4 +22,8 @@ class CreateArray:
             self.__array = np.random.randint(low=0, high=__size, size = __size+1)
         
     def create(self):
+        """
+        This function returns the generated array.
+        :return: numpy.array
+        """
         return self.__array
